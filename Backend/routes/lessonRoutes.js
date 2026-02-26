fefca589cd0a001f3a08c2e5f9fb938bd602e877
ngohuyhust <lagvay@gmail.com>
2026-02-26 22:50:09 +0700

    co lam frontend xoong thieu j thi bao

--- Backend/routes/lessonRoutes.js
+++ Backend/routes/lessonRoutes.js
@@ -4,9 +4,9 @@ import { authenticateToken } from '../utils/middleware.js';
 
 const router = express.Router();
 
-router.get('/themes', authenticateToken, ThemesController.getAllThemes); // tra ve danh sach cac chu de
-router.get('/themes/:themeId', authenticateToken, ThemesController.getThemesContent); // tra ve noi dung cua 1 chu de(gom co thong tin chu de va thong tin cac bai hoc)
-router.get('/themes/:themeId/lesson/:lessonId', authenticateToken, LessonsController.getLessonsContent); // tra ve noi dung cua 1 bai hoc(doan van)
-router.get('/themes/:themeId/lesson/:lessonId/vocabularies', authenticateToken, VocabulariesController.getVocabulariesByLessonId); // tra ve danh sach tu vung cua 1 bai hoc
+router.get('/', authenticateToken, ThemesController.getAllThemes); // tra ve danh sach cac chu de
+router.get('/:themeId', authenticateToken, ThemesController.getThemesContent); // tra ve noi dung cua 1 chu de(gom co thong tin chu de va thong tin cac bai hoc)
+router.get('/:themeId/:lessonId', authenticateToken, LessonsController.getLessonsContent); // tra ve noi dung cua 1 bai hoc(doan van)
+router.get('/:themeId/:lessonId/vocabularies', authenticateToken, VocabulariesController.getVocabulariesByLessonId); // tra ve danh sach tu vung cua 1 bai hoc
 
 export default router;
