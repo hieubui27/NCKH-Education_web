7c9b80f8de06df9476f50d48ebc26d990ac0e568
ngohuyhust <lagvay@gmail.com>
2026-03-02 17:15:15 +0700

    abc

--- Backend/routes/lessonRoutes.js
+++ Backend/routes/lessonRoutes.js
@@ -4,11 +4,11 @@ import { authenticateToken } from '../utils/middleware.js';
 
 const router = express.Router();
 
-router.get('/search', SearchController.suggest); // tim kiem tu khoa
+router.get('/search', authenticateToken, SearchController.suggest); // tim kiem tu khoa
 
 router.get('/', authenticateToken, ThemesController.getAllThemes); // tra ve danh sach cac chu de
 router.get('/:themeId', authenticateToken, ThemesController.getThemesContent); // tra ve noi dung cua 1 chu de(gom co thong tin chu de va thong tin cac bai hoc)
-router.get('/:lessonId',  LessonsController.getLessonsContent); // tra ve noi dung cua 1 bai hoc(doan van)
-router.get('/:wordId', authenticateToken, VocabulariesController.getVocabulariesByWordId); // tra ve danh sach tu vung cua 1 bai hoc
+router.get('/:lessonId', authenticateToken, LessonsController.getLessonsContent); // tra ve noi dung cua 1 bai hoc(doan van va tu vung cua no)
+router.get('/:wordId', authenticateToken, VocabulariesController.getVocabulariesByWordId); // tra ve noi dung tu vung
 
 export default router;
